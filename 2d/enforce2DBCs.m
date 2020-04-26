bc36e4e0dbcaa67805971507678677cdb5ed91c2
Liam C <liamc628@gmail.com>
2020-04-25 22:33:37 -0400

    plots, dirichlet error fixed

--- 2d/enforce2DBCs.m
+++ 2d/enforce2DBCs.m
@@ -1,19 +1,23 @@
 function [K,F] = enforce2DBCs(K,F,KofXY,boundaryValues,boundaryNodes)
-    numDirichlet = length(boundaryNodes('Dirichlet'));
-    nNodes = length(K);
-    dirichletValues = boundaryValues('Dirichlet');
-    dirichletNodes = boundaryNodes('Dirichlet');
-    
-    for i = 1:numDirichlet
-        currDirichlet = dirichletNodes(i);
-        K(currDirichlet,:)=zeros(1,nNodes);
-        K(currDirichlet,currDirichlet)=1;
-        F(currDirichlet)=dirichletValues(i);
-        
-        for j =1:nNodes %columns
-            F(j)=F(j)-K(j,currDirichlet)*dirichletValues(i);
-        end
-        
+numDirichlet = length(boundaryNodes('Dirichlet'));
+nNodes = length(K);
+dirichletValues = boundaryValues('Dirichlet');
+dirichletNodes = boundaryNodes('Dirichlet');
+
+for currDirichlet = 1:numDirichlet  
+    for j =1:nNodes %columns
+        F(j)=F(j)-K(j,currDirichlet)*dirichletValues(currDirichlet);
+        K(j,currDirichlet)=0;
     end
+end
+
+for currDirichlet = 1:numDirichlet
+    K(currDirichlet,:)=zeros(1,nNodes);
+    
+    K(currDirichlet,currDirichlet)=1;
+    F(currDirichlet)=dirichletValues(currDirichlet);
+    %disp(K(currDirichlet,:));
+    
+end
 
 end
